87f98f75346f6dec97c793f92ba47eff6eb4f9bc
TehKittyCat <tehkittycat@weirdgloop.org>
2023-12-10 10:42:12 -0500

    Remove $wgTmhEnableMp4Uploads, instead modify $wgTmhFileExtensions if mp4 support isn't wanted.

--- includes/RegistrationCallback.php
+++ includes/RegistrationCallback.php
@@ -12,14 +12,6 @@ class RegistrationCallback {
 	 */
 	public static function register( array $extInfo, SettingsBuilder $settingsBuilder ): void {
 		$tmhFileExtensions = $settingsBuilder->getConfig()->get( 'TmhFileExtensions' );
-
-		// Remove mp4 if not enabled:
-		if ( !$settingsBuilder->getConfig()->get( 'TmhEnableMp4Uploads' ) ) {
-			$index = array_search( 'mp4', $tmhFileExtensions, true );
-			if ( $index !== false ) {
-				array_splice( $tmhFileExtensions, $index, 1 );
-			}
-		}
 		$settingsBuilder->putConfigValue( MainConfigNames::FileExtensions, $tmhFileExtensions );
 
 		// Transcode jobs must be explicitly requested from the job queue:
