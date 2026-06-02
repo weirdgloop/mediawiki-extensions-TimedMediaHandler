157be3414fd12a13015f1660f3465cd1b4299277
TehKittyCat <tehkittycat@weirdgloop.org>
2026-06-02 17:39:57 -0400

    Only include one source for each codec.

--- includes/TimedMediaTransformOutput.php
+++ includes/TimedMediaTransformOutput.php
@@ -288,6 +288,23 @@ class TimedMediaTransformOutput extends MediaTransformOutput {
 		// constrained browsers (without js?) go with minimal source.)
 		usort( $mediaSources, [ $this, 'sortMediaByBandwidth' ] );
 
+		// WGL - Only include one source for each codec.
+		if ( count( $mediaSources ) > 1 ) {
+			$trimmedSources = [];
+			$seenCodecs = [];
+			foreach ( $mediaSources as &$source ) {
+				$codecs = ( $source['videoCodec'] ?? '' ) . ( $source['audioCodec'] ?? '' );
+				if ( in_array( $codecs, $seenCodecs ) ) {
+					continue;
+				}
+				if ( $codecs !== '' ) {
+					$seenCodecs[] = $codecs;
+				}
+				$trimmedSources[] = $source;
+			}
+			$mediaSources = $trimmedSources;
+		}
+
 		// We prefix some source attributes with data- to pass along to the javascript player
 		$prefixedSourceAttr = [
 			'width',
@@ -295,6 +312,7 @@ class TimedMediaTransformOutput extends MediaTransformOutput {
 			'transcodekey',
 		];
 		$removeSourceAttr = [
+			'audioCodec',
 			'bandwidth',
 			'framerate',
 			'disablecontrols',
@@ -302,6 +320,7 @@ class TimedMediaTransformOutput extends MediaTransformOutput {
 			'shorttitle',
 			'label',
 			'res',
+			'videoCodec',
 		];
 		foreach ( $mediaSources as &$source ) {
 			foreach ( $source as $attr => $val ) {
